53c897cbaac160c01e715954fcc9d097fe077941
recep <recep@TULPAR-REC-PC>
2017-04-19 19:26:14 +0300

    a

--- src/com/recep/entity/Araba.java
+++ src/com/recep/entity/Araba.java
@@ -1,5 +1,45 @@
 package com.recep.entity;
 
-public class Araba {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
+public class Araba {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	private String isim;
+	private String marka;
+	private String model;
+	
+	public Araba(){}
+	
+	public Araba(int id, String isim, String marka, String model) {
+		super();
+		this.id = id;
+		this.isim = isim;
+		this.marka = marka;
+		this.model = model;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getIsim() {
+		return isim;
+	}
+
+	public String getMarka() {
+		return marka;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	
 }
--- src/com/recep/entity/Otopark.java
+++ src/com/recep/entity/Otopark.java
@@ -1,5 +1,42 @@
 package com.recep.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+
+@Entity
 public class Otopark {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	
+	@OneToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "araba_id")
+	private Araba araba;
+	
+	public Otopark() {}
+	
+	public Otopark(int id) {
+		this.id = id;
+	}
+	
+	public int getId() {
+		return id;
+	}
+
+	public Araba getAraba() {
+		return araba;
+	}
+
+	public void setAraba(Araba araba) {
+		this.araba = araba;
+	}
+	
+	
 
 }
